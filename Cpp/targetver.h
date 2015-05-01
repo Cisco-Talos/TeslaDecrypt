cbc0eb9ff96c87a2ed5f6d50c647fafab9b9e045
shahurle <shahurle@cisco.com>
2015-05-01 11:35:50 -0400

    Update targetver.h
    Translation to English from Italian

--- Cpp/targetver.h
+++ Cpp/targetver.h
@@ -7,14 +7,19 @@
 
 // Modificare le seguenti definizioni se è necessario utilizzare come destinazione una piattaforma prima di quelle specificate di seguito.
 // Fare riferimento a MSDN per informazioni aggiornate sui valori corrispondenti per le differenti piattaforme.
+
+// The macros listed below specify the minimum versions of Windows and Internet Explorer required. 
+// These definitions can be changed if the target version if earlier than the ones currently specified. 
+// Please refer to the MSDn documentation to get the proper values for both the IE and Windows version.
+
 #ifndef WINVER                          // Specifica che la piattaforma minima richiesta è Windows Xp
 #define WINVER 0x0502           // Modificare il valore con quello appropriato per altre versioni di Windows.
-#endif
+#endif                          // The platform version minimum is currnetly set to 'Windows XP'
 
 #ifndef _WIN32_WINNT            // Specifica che la piattaforma minima richiesta è Windows Xp.
 #define _WIN32_WINNT 0x0502     // Modificare il valore con quello appropriato per altre versioni di Windows.
-#endif
+#endif                          // The platform version minimum is currnetly set to 'Windows XP'
 
-#ifndef _WIN32_IE                       // Specifica che la piattaforma minima richiesta è Internet Explorer 7.0.
+#ifndef _WIN32_IE               // Specifica che la piattaforma minima richiesta è Internet Explorer 7.0.
 #define _WIN32_IE 0x0700        // Modificare il valore con quello appropriato per altre versioni di IE.
-#endif
+#endif                          // The browser version is currnetly set to 'Internet Explorer 7.0'
